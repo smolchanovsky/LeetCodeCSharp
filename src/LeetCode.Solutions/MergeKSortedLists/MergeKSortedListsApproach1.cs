e5a1a6f96b74776479485524ab303cbc30840e65
smolchanovsky <smolchanovsky@gmail.com>
2020-05-02 22:58:14 +0500

    Add solution for Merge K Sorted Lists problem

---
+++ src/LeetCode.Solutions/MergeKSortedLists/MergeKSortedListsApproach1.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using LeetCode.Helpers;
+
+namespace LeetCode.Solutions.MergeKSortedLists
+{
+	/// <summary>
+	/// Iterative algorithm.
+	/// Time complexity: O(N * Log K) where K is the number of linked lists.
+	/// Space complexity: O(1).
+	/// </summary>
+	public class MergeKSortedListsApproach1 : IMergeKSortedLists
+	{
+		public ListNode Merge(ListNode[] lists)
+		{
+			var step = 1;
+			while (step < lists.Length)
+			{
+				for (var i = 0; i + step < lists.Length; i += step * 2)
+				{
+					lists[i] = Merge(lists[i], lists[i + step]);
+				}
+
+				step *= 2;
+			}
+
+			return lists.FirstOrDefault();
+		}
+
+		private ListNode Merge(ListNode first, ListNode second)
+		{
+			var root = new ListNode();
+			var currentResult = root;
+			ListNode? currentFirst = first;
+			ListNode? currentSecond = second;
+
+			while (currentFirst != null || currentSecond != null)
+			{
+				if (currentFirst == null || currentFirst.Value > currentSecond?.Value)
+				{
+					currentResult.Next = currentSecond!;
+					currentResult = currentResult.Next;
+					currentSecond = currentSecond!.Next;
+					continue;
+				}
+
+				if (currentSecond == null || currentFirst?.Value <= currentSecond.Value)
+				{
+					currentResult.Next = currentFirst!;
+					currentResult = currentResult.Next;
+					currentFirst = currentFirst!.Next;
+					continue;
+				}
+			}
+
+			return root.Next!;
+		}
+	}
+}
