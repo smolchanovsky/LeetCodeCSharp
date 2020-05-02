02cddf5babce5b1f6d733617eaf82110559ea8cb
smolchanovsky <smolchanovsky@gmail.com>
2020-05-02 22:57:10 +0500

    Add solution for Merge Two Sorted Lists problem

---
+++ src/LeetCode.Solutions/MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,9 @@
+﻿using LeetCode.Helpers;
+
+namespace LeetCode.Solutions.MergeTwoSortedLists
+{
+	public interface IMergeTwoSortedLists
+	{
+		ListNode Merge(ListNode first, ListNode second);
+	}
+}
