7a86778cb602f7775b788af875d749f258138193
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 09:26:00 +0500

    Add solution for Two Sum problem

---
+++ src/LeetCode.Solutions/TwoSum/TwoSum.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Solutions.TwoSum
+{
+	/// <summary>
+	/// <see href="https://leetcode.com/problems/two-sum/">Two Sum</see>
+	/// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+	/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+	/// </summary>
+	public interface ITwoSum
+	{
+		int[] Get(int[] nums, int target);
+	}
+}
