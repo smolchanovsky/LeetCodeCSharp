619bd2a6fe73719c9aa65e2995d9120d85d3994b
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 22:22:44 +0500

    Add test for Linked List Cycle problem

---
+++ src/LeetCode.Solutions/LinkedListCycle/LinkedListCycleApproach1.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Helpers;
+
+namespace LeetCode.Solutions.LinkedListCycle
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class LinkedListCycleApproach1 : ILinkedListCycle
+	{
+		public bool HasCycle(ListNode head)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
