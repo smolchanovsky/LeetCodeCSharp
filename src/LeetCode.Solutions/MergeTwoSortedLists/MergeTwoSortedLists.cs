184f9b35131b0cbdf34ce02bc06fcd3d5b41292b
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 09:24:51 +0500

    Add description for Merge Two Sorted Lists

--- src/LeetCode.Solutions/MergeTwoSortedLists/MergeTwoSortedLists.cs
+++ src/LeetCode.Solutions/MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -2,6 +2,11 @@
 
 namespace LeetCode.Solutions.MergeTwoSortedLists
 {
+	/// <summary>
+	/// <see href="https://leetcode.com/problems/merge-two-sorted-lists/">Merge Two Sorted Lists</see>
+	/// Merge two sorted linked lists and return it as a new list.
+	/// The new list should be made by splicing together the nodes of the first two lists.
+	/// </summary>
 	public interface IMergeTwoSortedLists
 	{
 		ListNode Merge(ListNode first, ListNode second);
