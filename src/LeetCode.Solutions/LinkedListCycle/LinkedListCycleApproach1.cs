c489f9dd10777fa06d50ea8c8dabea6f6ca6da47
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 22:31:45 +0500

    Add solution for Linked List Cycle problem

--- src/LeetCode.Solutions/LinkedListCycle/LinkedListCycleApproach1.cs
+++ src/LeetCode.Solutions/LinkedListCycle/LinkedListCycleApproach1.cs
@@ -3,13 +3,29 @@
 namespace LeetCode.Solutions.LinkedListCycle
 {
 	/// <summary>
-	/// 
+	/// Time complexity: O(n).
+	/// Space complexity: O(1).
 	/// </summary>
 	public class LinkedListCycleApproach1 : ILinkedListCycle
 	{
 		public bool HasCycle(ListNode head)
 		{
-			throw new System.NotImplementedException();
+			if (head?.Next == null)
+				return false;
+			
+			var slowPointer = head;
+			var fastPointer = head.Next;
+			
+			while (slowPointer != fastPointer)
+			{
+				if (fastPointer?.Next == null)
+					return false;
+
+				slowPointer = slowPointer.Next!;
+				fastPointer = fastPointer.Next.Next;
+			}
+
+			return true;
 		}
 	}
 }
