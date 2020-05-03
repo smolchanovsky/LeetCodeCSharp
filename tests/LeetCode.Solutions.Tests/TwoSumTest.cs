b5ffee168f755e16550085344a15565f8a9b3ea0
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 09:25:44 +0500

    Add tests for Two Sum

---
+++ tests/LeetCode.Solutions.Tests/TwoSumTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using FluentAssertions;
+using LeetCode.Solutions.TwoSum;
+using NUnit.Framework;
+
+namespace LeetCode.Solutions.Tests
+{
+	[TestFixture]
+	public class TwoSumTest
+	{
+		private static IEnumerable testCases = new[]
+		{
+			new TestCaseData(Array.Empty<int>(), 2, Array.Empty<int>())
+				.SetName("Empty array"),
+			new TestCaseData(new[] {1}, 1, Array.Empty<int>())
+				.SetName("Array with single item"),
+			new TestCaseData(new[] {1, 2}, 3, new[] {0, 1})
+				.SetName("Array with two items"),
+			new TestCaseData(new[] {1, 2, 3, 5, 4}, 6, new[] {0, 3})
+				.SetName("Array with many items"),
+			new TestCaseData(new[] {2, 1, 5, 4, 3}, 5, new[] {1, 3})
+				.SetName("Target is same as array item"),
+			new TestCaseData(new[] {2, 7, 3, -1}, 6, new[] {1, 3})
+				.SetName("Array with negative items")
+		};
+		
+		private ITwoSum twoSumApproach1;
+
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			twoSumApproach1 = new TwoSumApproach1();
+		}
+
+		[TestCaseSource(nameof(testCases))]
+		public void Approach1_Get(int[] nums, int target, int[] expectedResult)
+		{
+			var actualResult = twoSumApproach1.Get(nums, target);
+			
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
