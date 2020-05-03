7e90c2f8c2cf4f1156ca89760f7732e58fcb379a
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 09:52:27 +0500

    Add Longest Substring Without Repeating Characters problem

---
+++ src/LeetCode.Solutions/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersApproach1.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Solutions.LongestSubstringWithoutRepeatingCharacters
+{
+	public class LongestSubstringWithoutRepeatingCharactersApproach1 : ILongestSubstringWithoutRepeatingCharacters
+	{
+		public int GetLength(string str)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
