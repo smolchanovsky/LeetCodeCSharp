42ad2b251fcb14892d9d686eed5a61073f8d4026
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 10:28:43 +0500

    Add solution for Longest Substring Without Repeating Characters problem

--- src/LeetCode.Solutions/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersApproach1.cs
+++ src/LeetCode.Solutions/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersApproach1.cs
@@ -1,10 +1,32 @@
-﻿namespace LeetCode.Solutions.LongestSubstringWithoutRepeatingCharacters
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.LongestSubstringWithoutRepeatingCharacters
 {
 	public class LongestSubstringWithoutRepeatingCharactersApproach1 : ILongestSubstringWithoutRepeatingCharacters
 	{
 		public int GetLength(string str)
 		{
-			throw new System.NotImplementedException();
+			var indicesByChars = new Dictionary<char, int>();
+			var maxLength = 0;
+			
+			for (var currentIndex = 0; currentIndex < str.Length; currentIndex++)
+			{
+				var character = str[currentIndex];
+				
+				if (indicesByChars.TryGetValue(character, out var rollbackIndex))
+				{
+					indicesByChars.Clear();
+					currentIndex = rollbackIndex;
+					continue;
+				}
+
+				indicesByChars[character] = currentIndex;
+
+				if (indicesByChars.Count > maxLength)
+					maxLength = indicesByChars.Count;
+			}
+
+			return maxLength;
 		}
 	}
 }
