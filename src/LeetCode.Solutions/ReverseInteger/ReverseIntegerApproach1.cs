a7cec061e7fde0df7aaf5a08de3e79328b4d6d49
smolchanovsky <smolchanovsky@gmail.com>
2020-05-03 12:36:36 +0500

    Add solution for Reverse Integer problem

---
+++ src/LeetCode.Solutions/ReverseInteger/ReverseIntegerApproach1.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Solutions.ReverseInteger
+{
+	/// <summary>
+	/// Time Complexity: O(log(x)). There are roughly log10(x) digits in x.
+	/// Space Complexity: O(1).
+	/// </summary>
+	public class ReverseIntegerApproach1 : IReverseInteger
+	{
+		private const int intMaxValueLastDigit = 7;
+		private const int intMinValueLastDigit = 8;
+		
+		public int Reverse(int number)
+		{
+			var result = 0;
+
+			while (number != 0)
+			{
+				var pop = number % 10;
+				if (WillOverflow(result, pop)) 
+					return 0;
+
+				number /= 10;
+				result = checked(result * 10 + pop);
+			}
+
+			return result;
+		}
+
+		private static bool WillOverflow(int currentResult, int currentPop)
+		{
+			if (currentResult > int.MaxValue / 10 || (currentResult == int.MaxValue / 10 && currentPop > intMaxValueLastDigit)) 
+				return true;
+			if (currentResult < int.MinValue / 10 || (currentResult == int.MinValue / 10 && currentPop < -intMinValueLastDigit)) 
+				return true;
+
+			return false;
+		}
+	}
+}
