0c6ac944666dd7d93ad20f60393d192f4db47bc9
smolchanovsky <smolchanovsky@gmail.com>
2020-05-04 11:01:11 +0500

    Add solution for Best Time To Buy And Sell Stock problem

--- src/LeetCode.Solutions/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockApproach1.cs
+++ src/LeetCode.Solutions/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockApproach1.cs
@@ -1,13 +1,30 @@
 ﻿namespace LeetCode.Solutions.BestTimeToBuyAndSellStock
 {
 	/// <summary>
-	/// 
+	/// Time complexity: O(n).
+	/// Space complexity: O(1).
 	/// </summary>
 	public class BestTimeToBuyAndSellStockApproach1 : IBestTimeToBuyAndSellStock
 	{
 		public int GetMaxProfit(int[] prices)
 		{
-			throw new System.NotImplementedException();
+			if (prices.Length == 0)
+				return 0;
+			
+			var minPrice = prices[0];
+			var maxProfit = 0;
+			
+			foreach (var price in prices)
+			{
+				if (minPrice > price)
+					minPrice = price;
+				
+				var profit = price - minPrice;
+				if (maxProfit < profit)
+					maxProfit = profit;
+			}
+
+			return maxProfit;
 		}
 	}
 }
