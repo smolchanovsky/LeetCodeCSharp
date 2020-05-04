a4c010877bd887708d75c756f582cc61f5f59262
smolchanovsky <smolchanovsky@gmail.com>
2020-05-04 11:39:50 +0500

    Add solution for Valid Anagram problem

--- src/LeetCode.Solutions/ValidAnagram/ValidAnagramApproach1.cs
+++ src/LeetCode.Solutions/ValidAnagram/ValidAnagramApproach1.cs
@@ -1,13 +1,30 @@
-﻿namespace LeetCode.Solutions.ValidAnagram
+﻿using System.Linq;
+
+namespace LeetCode.Solutions.ValidAnagram
 {
 	/// <summary>
-	/// 
+	/// Time complexity: O(n).
+	/// Space complexity: O(1).
 	/// </summary>
 	public class ValidAnagramApproach1 : IValidAnagram
 	{
 		public bool IsAnagram(string first, string second)
 		{
-			throw new System.NotImplementedException();
+			if (first.Length != second.Length)
+				return false;
+			
+			var charCounter = new int['z' - 'a' + 1];
+
+			for (var i = 0; i < first.Length; i++)
+			{
+				var firstCharIndex = 'z' - first[i];
+				++charCounter[firstCharIndex];
+				
+				var secondCharIndex = 'z' - second[i];
+				--charCounter[secondCharIndex];
+			}
+
+			return charCounter.All(x => x == 0);
 		}
 	}
 }
