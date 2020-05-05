79fed3e40663a62df22e8f05ec7edd0267ee20aa
smolchanovsky <smolchanovsky@gmail.com>
2020-05-05 22:36:11 +0500

    Add approach 1 for Reverse Linked List problem

--- src/LeetCode.Solutions/ReverseLinkedList/ReverseLinkedListApproach1.cs
+++ src/LeetCode.Solutions/ReverseLinkedList/ReverseLinkedListApproach1.cs
@@ -3,13 +3,26 @@
 namespace LeetCode.Solutions.ReverseLinkedList
 {
 	/// <summary>
-	/// 
+	/// Iterative algorithm.
+	/// Time complexity: O(n).
+	/// Space complexity: O(1).
 	/// </summary>
 	public class ReverseLinkedListApproach1 : IReverseLinkedList
 	{
 		public ListNode Reverse(ListNode head)
 		{
-			throw new System.NotImplementedException();
+			ListNode? result = null;
+			var currentNode = head;
+
+			while (currentNode != null)
+			{
+				var nextNode = currentNode.Next;
+				currentNode.Next = result;
+				result = currentNode;
+				currentNode = nextNode;
+			}
+
+			return result!;
 		}
 	}
 }
