3ee41aa36958ea8eadf34fb01fde171df44b74fd
smolchanovsky <smolchanovsky@gmail.com>
2020-09-19 00:17:43 +0300

    Add solution for Remove Duplicates From Sorted Array problem

--- src/LeetCode.Solutions/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayApproach1.cs
+++ src/LeetCode.Solutions/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayApproach1.cs
@@ -1,16 +1,27 @@
-﻿using System;
-
-namespace LeetCode.Solutions.RemoveDuplicatesFromSortedArray
+﻿namespace LeetCode.Solutions.RemoveDuplicatesFromSortedArray
 {
 	/// <summary>
-	/// Time Complexity: .
+	/// Time Complexity: O(n).
 	/// Space Complexity: O(1).
 	/// </summary>
 	public class RemoveDuplicatesFromSortedArrayApproach1 : IRemoveDuplicatesFromSortedArray
 	{
 		public int RemoveDuplicates(int[] numbers)
 		{
-			throw new NotImplementedException();
+			if (numbers.Length == 0)
+				return 0;
+
+			var newIndex = 0;
+			for (var i = 1; i < numbers.Length; i++)
+			{
+				if (numbers[newIndex] == numbers[i]) 
+					continue;
+				
+				++newIndex;
+				numbers[newIndex] = numbers[i];
+			}
+
+			return newIndex + 1;
 		}
 	}
 }
