6029513495f787d8315ec205fba3c9823a00a6ce
smolchanovsky <smolchanovsky@gmail.com>
2020-09-26 15:26:38 +0300

    Add approach for implement strStr() problem

--- src/LeetCode.Solutions/ImplementStrStr/ImplementStrStrApproach1.cs
+++ src/LeetCode.Solutions/ImplementStrStr/ImplementStrStrApproach1.cs
@@ -1,20 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace LeetCode.Solutions.ImplementStrStr
 {
     /// <summary>
-	/// Time complexity: O(...).
-	/// Space complexity: O(...).
+	/// Time complexity: O(n * m) where n is haystack length and m is needle length.
+	/// Space complexity: O(m).
+	/// Runtime: 72 ms, faster than 91.05% of C# online submissions for Implement strStr().
+	/// Memory Usage: 23 MB, less than 21.18% of C# online submissions for Implement strStr().
 	/// </summary>
     public class ImplementStrStrApproach1 : IImplementStrStr
     {
         public int StrStr(string haystack, string needle)
-        {
-            throw new NotImplementedException();
-        }
+		{
+			for (var i = 0; i < haystack.Length - needle.Length + 1; i++)
+			{
+				var needleCandidate = haystack[i..(i + needle.Length)];
+				if (needleCandidate == needle)
+					return i;
+			}
+
+			return -1;
+		}
     }
 }
