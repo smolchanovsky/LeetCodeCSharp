0b324b4bf45f43ca10021c1cbb30df03e90e90ad
smolchanovsky <smolchanovsky@gmail.com>
2020-09-26 23:54:39 +0300

    Add tests for plus one problem

--- tests/LeetCode.Solutions.Tests/PlusOneTest.cs
+++ tests/LeetCode.Solutions.Tests/PlusOneTest.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using LeetCode.Helpers;
 using LeetCode.Solutions.PlusOne;
 using NUnit.Framework;
 using FluentAssertions;
@@ -13,7 +8,23 @@ namespace LeetCode.Solutions.Tests
     [TestFixture]
     public class PlusOneTest
     {
-        private static IEnumerable testCases = new[] { new TestCaseData().SetName("") };
+        private static IEnumerable testCases = new[]
+        {
+            new TestCaseData(new[] { 0 }, new[] { 1 })
+                .SetName("Zero"),
+            new TestCaseData(new[] { 9 }, new[] { 1, 0 })
+                .SetName("Addition with carry: one digit number"),
+            new TestCaseData(new[] { 9, 9 }, new[] { 1, 0, 0 })
+                .SetName("Addition with carry: two digit number"),
+            new TestCaseData(new[] { 1, 2, 3 }, new[] { 1, 2, 4 })
+                .SetName("Multiple digits number"),
+            new TestCaseData(new[] { 9, 9, 9 }, new[] { 1, 0, 0, 0 })
+                .SetName("Addition with carry: multiple digits number"),
+            new TestCaseData(new[] { 1, 0, 0, 0 }, new[] { 1, 0, 0, 1 })
+                .SetName("Round number"),
+            new TestCaseData(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 1 })
+                .SetName("Big multiple digits number"),
+        };
 
         private IPlusOne approach1;
 
@@ -26,9 +37,9 @@ namespace LeetCode.Solutions.Tests
         [TestCaseSource(nameof(testCases))]
         public void Approach1_PlusOne(int[] digits, int[] expectedResult)
         {
-            var actualResult = approach1.PlusOne();
+            var actualResult = approach1.PlusOne(digits);
 
-            actualResult.Should().Be(expectedResult);
+            actualResult.Should().BeEquivalentTo(expectedResult);
         }
     }
 }
