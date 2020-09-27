a38acd2a1eb7960bff0a2d3c10e52b3881e7c41a
smolchanovsky <smolchanovsky@gmail.com>
2020-09-27 23:11:58 +0300

    Add tests for single number problem

--- tests/LeetCode.Solutions.Tests/SingleNumberTest.cs
+++ tests/LeetCode.Solutions.Tests/SingleNumberTest.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using LeetCode.Helpers;
 using LeetCode.Solutions.SingleNumber;
 using NUnit.Framework;
 using FluentAssertions;
@@ -13,7 +8,21 @@ namespace LeetCode.Solutions.Tests
     [TestFixture]
     public class SingleNumberTest
     {
-        private static IEnumerable testCases = new[] { new TestCaseData().SetName("") };
+        private static IEnumerable testCases = new[]
+        {
+            new TestCaseData(new[] { 1 }, 1)
+                .SetName("Array with single item"),
+            new TestCaseData(new[] { 2, 2, 1 }, 1)
+                .SetName("Array with one repeating item"),
+            new TestCaseData(new[] { 4, 1, 2, 1, 2 }, 4)
+                .SetName("Array with multiple repeating items"),
+            new TestCaseData(new[] { 4, -1, 2, -1, 2 }, 4)
+                .SetName("Array with multiple repeating negative items"),
+            new TestCaseData(new[] { 3, 3, -4, 2, 2 }, -4)
+                .SetName("Negative non-repeating item"),
+            new TestCaseData(new[] { 43894329, 1493049, 23918343, 1493049, 23918343 }, 43894329)
+                .SetName("Array with big numbers")
+        };
 
         private ISingleNumber approach1;
 
